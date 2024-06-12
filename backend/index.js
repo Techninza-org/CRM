98ddda654686719ec3c342d9570feffc40c042bd
amit04-07-1999 <ameykumar76@gmail.com>
2024-06-12 11:09:47 +0530

    build-file

--- backend/index.js
+++ backend/index.js
@@ -23,11 +23,11 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static("./uploads"));
 
-// app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, 'dist')));
 
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
-// });
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
 
 // MongoDB setup
 const url = process.env.MONGODB_URI;
